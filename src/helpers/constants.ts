ca11f9732f74a6645829a1c360096637a0e56ba4
wood.wu <kilokingw@gmail.com>
2024-11-29 21:36:35 +0800

    Update constants.ts

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -65,77 +65,37 @@ export const getImageUrl = (base: keyof typeof IMAGE_URLS, path: string) => {
 // S3MBC03
 // S3MBC04
 export const HOME_DATA = [
-  // Series 1 Female White
+  // Group 1 (C01)
   { id: "S1FW1", img: "S1FWC01.png" },
-  { id: "S1FW2", img: "S1FWC02.png" },
-  { id: "S1FW3", img: "S1FWC03.png" },
-  { id: "S1FW4", img: "S1FWC04.png" },
-
-  // Series 2 Female White
-  { id: "S2FW1", img: "S2FWC01.png" },
-  { id: "S2FW2", img: "S2FWC02.png" },
-  { id: "S2FW3", img: "S2FWC03.png" },
-  { id: "S2FW4", img: "S2FWC04.png" },
-
-  // Series 3 Female White
-  { id: "S3FW1", img: "S3FWC01.png" },
-  { id: "S3FW2", img: "S3FWC02.png" },
-  { id: "S3FW3", img: "S3FWC03.png" },
-  { id: "S3FW4", img: "S3FWC04.png" },
-
-  // Series 1 Male White
-  { id: "S1MW1", img: "S1MWC01.png" },
-  { id: "S1MW2", img: "S1MWC02.png" },
-  { id: "S1MW3", img: "S1MWC03.png" },
-  { id: "S1MW4", img: "S1MWC04.png" },
-
-  // Series 2 Male White
-  { id: "S2MW1", img: "S2MWC01.png" },
-  { id: "S2MW2", img: "S2MWC02.png" },
-  { id: "S2MW3", img: "S2MWC03.png" },
-  { id: "S2MW4", img: "S2MWC04.png" },
-
-  // Series 3 Male White
-  { id: "S3MW1", img: "S3MWC01.png" },
-  { id: "S3MW2", img: "S3MWC02.png" },
-  { id: "S3MW3", img: "S3MWC03.png" },
-  { id: "S3MW4", img: "S3MWC04.png" },
-
-  // Series 1 Female Black
-  { id: "S1FB1", img: "S1FBC01.png" },
-  { id: "S1FB2", img: "S1FBC02.png" },
-  { id: "S1FB3", img: "S1FBC03.png" },
-  { id: "S1FB4", img: "S1FBC04.png" },
-
-  // Series 2 Female Black
-  { id: "S2FB1", img: "S2FBC01.png" },
-  { id: "S2FB2", img: "S2FBC02.png" },
-  { id: "S2FB3", img: "S2FBC03.png" },
-  { id: "S2FB4", img: "S2FBC04.png" },
-
-  // Series 3 Female Black
-  { id: "S3FB1", img: "S3FBC01.png" },
-  { id: "S3FB2", img: "S3FBC02.png" },
-  { id: "S3FB3", img: "S3FBC03.png" },
-  { id: "S3FB4", img: "S3FBC04.png" },
-
-  // Series 1 Male Black
   { id: "S1MB1", img: "S1MBC01.png" },
-  { id: "S1MB2", img: "S1MBC02.png" },
-  { id: "S1MB3", img: "S1MBC03.png" },
-  { id: "S1MB4", img: "S1MBC04.png" },
-
-  // Series 2 Male Black
+  { id: "S2FW1", img: "S2FWC01.png" },
   { id: "S2MB1", img: "S2MBC01.png" },
-  { id: "S2MB2", img: "S2MBC02.png" },
-  { id: "S2MB3", img: "S2MBC03.png" },
-  { id: "S2MB4", img: "S2MBC04.png" },
-
-  // Series 3 Male Black
+  { id: "S3FW1", img: "S3FWC01.png" },
   { id: "S3MB1", img: "S3MBC01.png" },
-  { id: "S3MB2", img: "S3MBC02.png" },
+
+  // Group 2 (C02)
+  { id: "S1FB2", img: "S1FBC02.png" },
+  { id: "S1MW2", img: "S1MWC02.png" },
+  { id: "S2FB2", img: "S2FBC02.png" },
+  { id: "S2MW2", img: "S2MWC02.png" },
+  { id: "S3FB2", img: "S3FBC02.png" },
+  { id: "S3MW2", img: "S3MWC02.png" },
+
+  // Group 3 (C03)
+  { id: "S1FW3", img: "S1FWC03.png" },
+  { id: "S1MB3", img: "S1MBC03.png" },
+  { id: "S2FW3", img: "S2FWC03.png" },
+  { id: "S2MB3", img: "S2MBC03.png" },
+  { id: "S3FW3", img: "S3FWC03.png" },
   { id: "S3MB3", img: "S3MBC03.png" },
-  { id: "S3MB4", img: "S3MBC04.png" },
+
+  // Group 4 (C04)
+  { id: "S1FB4", img: "S1FBC04.png" },
+  { id: "S1MW4", img: "S1MWC04.png" },
+  { id: "S2FB4", img: "S2FBC04.png" },
+  { id: "S2MW4", img: "S2MWC04.png" },
+  { id: "S3FB4", img: "S3FBC04.png" },
+  { id: "S3MW4", img: "S3MWC04.png" },
 ];
 
 export const SERIES_DATA = [
