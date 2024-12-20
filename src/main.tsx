99597ef6a12cd160e0576b715c99d1af00a30203
wood.wu <kilokingw@gmail.com>
2024-12-21 00:24:25 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 註釋掉應用程式的渲染
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// 只渲染一個空白頁面
+createRoot(document.getElementById("root")!).render(
+  <div style={{ width: "100vw", height: "100vh", background: "#000" }} />
+);
