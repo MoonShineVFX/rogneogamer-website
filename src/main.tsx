2dbb596abf9181949294177d3cceba63e1edb40b
wood.wu <kilokingw@gmail.com>
2025-01-03 12:04:28 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 
 // 註釋掉應用程式的渲染
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <div style={{ width: "100vw", height: "100vh", background: "#000" }} />
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>
 );
